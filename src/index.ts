ef82875aa42af9814406dcfa11aeec6bc508d724
Abhinav Thapa <abhinav.thapa@birdeye.com>
2025-03-18 17:16:03 +0530

    BIRD-00: logger added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { createWriteStream } from "fs";
 import { initClient } from "./config.js";
+import pino from "pino";
 
 const DAYS_IN_SECONDS = 86400;
 const SCAN_BATCH_SIZE = 5000;
 const SELECTED_DB = Number(process.env.REDIS_DB);
+const logger = pino.pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true
+    }
+  }
+});
 // const THIRTY_DAYS_IN_SECONDS = 1 * DAYS_IN_SECONDS;
 
   async function main() {
@@ -46,18 +55,18 @@ const SELECTED_DB = Number(process.env.REDIS_DB);
               (ttl ?? 0) / DAYS_IN_SECONDS
             ),
           };
-        })?.filter((item) => (item?.ttl === -1));
+        });
       const keysWithoutTTL = result?.filter((item) => (item?.ttl === -1));
       const oldKeyAccessed = result?.filter((item) => (item?.lastAccessedDays >= 7));
-      const isOldKeysPresent = (result?.length || 0) > 0;
+      const isOldKeysPresent = (oldKeyAccessed?.length || 0) > 0;
+      const noTTLKeysFound = keysWithoutTTL?.length || 0;
       log1.write(JSON.stringify(keysWithoutTTL));
       log2.write(JSON.stringify(oldKeyAccessed));
       log1.write("\n");
       log2.write("\n");
       // await writeFile(`run-result.txt`, JSON.stringify(result));  
-      console.log(
-        `<== Result: ${isOldKeysPresent ? "Found old keys" : "No old keys found"} - cursor=${cursor} - run=${runs} duration - ${(Date.now() - startTime) / 1000}seconds  ==>`,
-        result
+      logger.info(
+        `<== Result: oldKeysFound=${oldKeyAccessed?.length || 0} noTTLKeysFound=${noTTLKeysFound} - cursor=${cursor} - run=${runs} duration - ${(Date.now() - startTime) / 1000}seconds  ==>`
       );
     }
 
@@ -68,7 +77,7 @@ const SELECTED_DB = Number(process.env.REDIS_DB);
     } while (cursor !== 0);
     log1.end();
     log2.end();
-    console.log("Exiting process gracefully..");
+    logger.info("Exiting process gracefully..");
     process.exit(0);
   }  
   main();
