e2718d96a22de068457e668d4f5bc968f2599e4b
Abhinav Thapa <abhinav.thapa@birdeye.com>
2025-03-21 07:13:21 +0530

    BIRD-00: formatting and linting

--- src/config.ts
+++ src/config.ts
@@ -2,44 +2,44 @@ import type { RedisOptions } from "ioredis";
 import { Redis } from "ioredis";
 
 export const redisConfig = {
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
-  maxRetriesPerRequest: 1,
-  enableReadyCheck: true,
-  db: Number(process.env.REDIS_DB),
-  commandTimeout: 10000,
-  connectTimeout: 10000,
+	host: process.env.REDIS_HOST,
+	port: Number(process.env.REDIS_PORT),
+	password: process.env.REDIS_PASSWORD,
+	maxRetriesPerRequest: 1,
+	enableReadyCheck: true,
+	db: Number(process.env.REDIS_DB),
+	commandTimeout: 10000,
+	connectTimeout: 10000,
 } satisfies RedisOptions;
 
 export async function initClient() {
-  const client = new Redis(redisConfig);
-  client.on("connecting", () =>
-    console.log("Redis: Redis client connecting...")
-  );
-  client.on("ready", () => console.log("Redis: Redis client ready!"));
-  client.on("reconnecting", () =>
-    console.log("Redis: Redis client reconnecting...")
-  );
-  client.on("error", (error) =>
-    console.log("Redis: Redis client error", error)
-  );
-  client.on("connect", () => console.log("Redis: Redis client connected!"));
-  client.on("close", () => console.log("Redis: Redis client closed!"));
-  client.on("end", () => console.log("Redis: Redis client ended."));
-  
-  process.on("SIGTERM", ()=>{
-    console.log("Redis: Quitting as process exited");
-    client.quit();
-  });
-  process.on("SIGINT", ()=>{
-    console.log("Redis: Quitting as process exited");
-    client.quit();
-  });
-  process.on("exit", () => {
-    console.log("Redis: Quitting as process exited");
-    client.quit();
-  })
+	const client = new Redis(redisConfig);
+	client.on("connecting", () =>
+		console.log("Redis: Redis client connecting..."),
+	);
+	client.on("ready", () => console.log("Redis: Redis client ready!"));
+	client.on("reconnecting", () =>
+		console.log("Redis: Redis client reconnecting..."),
+	);
+	client.on("error", (error) =>
+		console.log("Redis: Redis client error", error),
+	);
+	client.on("connect", () => console.log("Redis: Redis client connected!"));
+	client.on("close", () => console.log("Redis: Redis client closed!"));
+	client.on("end", () => console.log("Redis: Redis client ended."));
 
-  return client;
+	process.on("SIGTERM", () => {
+		console.log("Redis: Quitting as process exited");
+		client.quit();
+	});
+	process.on("SIGINT", () => {
+		console.log("Redis: Quitting as process exited");
+		client.quit();
+	});
+	process.on("exit", () => {
+		console.log("Redis: Quitting as process exited");
+		client.quit();
+	});
+
+	return client;
 }
--- src/index.ts
+++ src/index.ts
@@ -6,91 +6,85 @@ const DAYS_IN_SECONDS = 86400;
 const SCAN_BATCH_SIZE = 5000;
 const SELECTED_DB = Number(process.env.REDIS_DB);
 const logger = pino.pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true
-    }
-  }
+	transport: {
+		target: "pino-pretty",
+		options: {
+			colorize: true,
+		},
+	},
 });
 // const THIRTY_DAYS_IN_SECONDS = 1 * DAYS_IN_SECONDS;
 
-  async function main() {
-    const client = await initClient();
-    let cursor = 0;
-    let runs = 0;
-    const log1 = createWriteStream('keys-without-ttl.txt', { flags: 'a' });
-    const log2 = createWriteStream('keys-older-than-7-days.txt', { flags: 'a' });
-    async function batchProcess(){
-      const startTime = Date.now();
-      const scanResult = await client.scan(cursor, "COUNT", SCAN_BATCH_SIZE);
-      const serverCursor = Number(scanResult?.[0] ?? 0);
-      cursor = serverCursor;
-      const keys = scanResult[1];
-      const commands: string[][] = [];
-      const ttlCommands: string[][] = [];
-    
-      for (const key of keys) {
-        commands.push(["object", "idletime", key]);
-        ttlCommands.push(["ttl", key]);
-      }
-      // const response = await client.pipeline(commands).exec();
-      const [idleTimeResponse, ttlResponse] = await Promise.all([
-        client.pipeline(commands).exec(),
-        client.pipeline(ttlCommands).exec(),
-      ]);
+async function main() {
+	const client = await initClient();
+	let cursor = 0;
+	let runs = 0;
+	const log1 = createWriteStream("keys-without-ttl.txt", { flags: "a" });
+	const log2 = createWriteStream("keys-older-than-7-days.txt", { flags: "a" });
+	async function batchProcess() {
+		const startTime = Date.now();
+		const scanResult = await client.scan(cursor, "COUNT", SCAN_BATCH_SIZE);
+		const serverCursor = Number(scanResult?.[0] ?? 0);
+		cursor = serverCursor;
+		const keys = scanResult[1];
+		const commands: string[][] = [];
+		const ttlCommands: string[][] = [];
 
+		for (const key of keys) {
+			commands.push(["object", "idletime", key]);
+			ttlCommands.push(["ttl", key]);
+		}
+		// const response = await client.pipeline(commands).exec();
+		const [idleTimeResponse, ttlResponse] = await Promise.all([
+			client.pipeline(commands).exec(),
+			client.pipeline(ttlCommands).exec(),
+		]);
 
-      const result = idleTimeResponse?.map((item, index) => {
-        const ttl =  Number(ttlResponse?.[index]?.[1]);
-        return {
-            db: SELECTED_DB,
-            key: commands?.[index]?.[2],
-            lastAccessedSeconds: Math.floor(Number(item?.[1] ?? 0) ?? null),
-            lastAccessedDays: Math.floor(
-              Number(item?.[1] ?? 0) / DAYS_IN_SECONDS
-            ),
-            ttl,
-            ttlInDays: Math.floor(
-              (ttl ?? 0) / DAYS_IN_SECONDS
-            ),
-          };
-        });
-      const keysWithoutTTL = result?.filter((item) => (item?.ttl === -1));
-      const oldKeyAccessed = result?.filter((item) => (item?.lastAccessedDays >= 7));
-      const oldKeysFound = (oldKeyAccessed?.length || 0);
-      const noTTLKeysFound = keysWithoutTTL?.length || 0;
-      log1.write(JSON.stringify(keysWithoutTTL));
-      log2.write(JSON.stringify(oldKeyAccessed));
-      log1.write("\n");
-      log2.write("\n");
-      logger.info(
-        {
-          cursor,
-          runs,
-          oldKeysFound:  oldKeysFound,
-          noTTLKeysFound: noTTLKeysFound,
-          duration: (Date.now() - startTime) / 1000
-        },
-        `Result: oldKeysFound=${
-          oldKeysFound
-        } noTTLKeysFound=${noTTLKeysFound} - cursor=${cursor} - run=${runs} duration - ${
-          (Date.now() - startTime) / 1000
-        }seconds`
-      );
-    }
+		const result = idleTimeResponse?.map((item, index) => {
+			const ttl = Number(ttlResponse?.[index]?.[1]);
+			return {
+				db: SELECTED_DB,
+				key: commands?.[index]?.[2],
+				lastAccessedSeconds: Math.floor(Number(item?.[1] ?? 0) ?? null),
+				lastAccessedDays: Math.floor(Number(item?.[1] ?? 0) / DAYS_IN_SECONDS),
+				ttl,
+				ttlInDays: Math.floor((ttl ?? 0) / DAYS_IN_SECONDS),
+			};
+		});
+		const keysWithoutTTL = result?.filter((item) => item?.ttl === -1);
+		const oldKeyAccessed = result?.filter(
+			(item) => item?.lastAccessedDays >= 7,
+		);
+		const oldKeysFound = oldKeyAccessed?.length || 0;
+		const noTTLKeysFound = keysWithoutTTL?.length || 0;
+		log1.write(JSON.stringify(keysWithoutTTL));
+		log2.write(JSON.stringify(oldKeyAccessed));
+		log1.write("\n");
+		log2.write("\n");
+		logger.info(
+			{
+				cursor,
+				runs,
+				oldKeysFound: oldKeysFound,
+				noTTLKeysFound: noTTLKeysFound,
+				duration: (Date.now() - startTime) / 1000,
+			},
+			`Result: oldKeysFound=${
+				oldKeysFound
+			} noTTLKeysFound=${noTTLKeysFound} - cursor=${cursor} - run=${runs} duration - ${
+				(Date.now() - startTime) / 1000
+			}seconds`,
+		);
+	}
 
-    do {
-      await batchProcess();
-      runs ++;
-      await new Promise(resolve => setTimeout(resolve, 500));
-    } while (cursor !== 0);
-    log1.end();
-    log2.end();
-    logger.info("Exiting process gracefully..");
-    process.exit(0);
-  }  
-  main();
-
-
-
+	do {
+		await batchProcess();
+		runs++;
+		await new Promise((resolve) => setTimeout(resolve, 500));
+	} while (cursor !== 0);
+	log1.end();
+	log2.end();
+	logger.info("Exiting process gracefully..");
+	process.exit(0);
+}
+main();
